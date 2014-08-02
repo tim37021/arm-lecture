7192c79eded69c296680c8b26c56a5562db35766
Tim Hsu <tim37021@gmail.com>
2014-08-02 14:32:06 +0800

    Answer to the assignment and some reference

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -9,6 +9,25 @@
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
+	@ Reference: http://electronics.stackexchange.com/questions/25690/critical-sections-on-cortex-m3
+	@ Reference: http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dht0008a/ch01s03s02.html
+
+	push {r1, r2}
+	ldr r2, =locked
+.WAIT_LOCK:
+	@Reference book: Introduction to Parallel Programming, you can find this book at http://prdrklaina.weebly.com/uploads/5/7/7/3/5773421/an_introduction_to_parallel_programming_-_peter_s._pacheco.pdf
+	@check if it is locked, wait until find it unlocked
+	ldrex r1, [r0]
+	cmp r1, #locked
+	beq .WAIT_LOCK
+
+	@try to use the lock, needs to check if locked again because context-switch may happen here
+	strex r1, r2, [r0]
+	@if fails, waiting again
+	cmp r1, #1
+	beq .WAIT_LOCK
+
+	pop {r1, r2}
 
         @ END CODE INSERT
 	bx lr
@@ -20,6 +39,11 @@ lock_mutex:
 unlock_mutex:
 	@ INSERT CODE BELOW
         
+	push {r1}
+	mov r1, #unlocked
+	str r1, [r0]
+	pop {r1}
+
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
